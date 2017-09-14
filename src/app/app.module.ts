0a04bfd6cd062f6f0a25c59f7dabf7d959823312
Hernan Y.Ke <yuriqiao@gmail.com>
2017-09-13 23:08:43 -0400

    Add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
 import { AppComponent } from './app.component';
+
+
+ const firebaseConfig = {
+    apiKey: "AIzaSyD66Bev_bJmrL1UPMxv71F1mExGvc26AOY",
+    authDomain: "psbuiappangularfire2.firebaseapp.com",
+    databaseURL: "https://psbuiappangularfire2.firebaseio.com",
+    projectId: "psbuiappangularfire2",
+    storageBucket: "psbuiappangularfire2.appspot.com",
+    messagingSenderId: "73705825948"
+  };
 
 @NgModule({
   declarations: [
@@ -12,7 +23,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
